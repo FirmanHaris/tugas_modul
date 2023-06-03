abfa1492f15451a07cb0db04884b0261f800d198
FirmanHaris <firmanharis281101@gmail.com>
2023-06-03 15:58:31 +0800

    tugas module

---
+++ index.py
@@ -0,0 +1,4 @@
+# memanggil module rumus
+import rumus
+print("Luas Segitiga : ",rumus.luas_segitiga(2,5))
+print("Luas Persegi Panjang : ",rumus.luas_persegipanjang(2,5))
---
+++ rumus.py
@@ -0,0 +1,6 @@
+def luas_segitiga(a,t):
+    luas = (a * t)/2
+    return luas
+def luas_persegipanjang(p,s):
+    luas = p*s
+    return luas
